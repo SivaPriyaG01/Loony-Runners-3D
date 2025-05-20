208e56135b25e7143f77886057c0e6b93948d2b7
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-05-20 23:19:41 +0530

    Update PlayerMovement.cs script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,45 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    
+    private int playerSpeed;
+    public int PlayerSpeed
+    {
+        get => playerSpeed;
+        set
+        {
+            playerSpeed += value;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerInput();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void PlayerInput()
+    {
+
+    }
+
+    void MovePlayer()
+    {
+
     }
 }
