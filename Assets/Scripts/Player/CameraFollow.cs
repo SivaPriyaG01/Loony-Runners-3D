46ed2550d54c0ffa3bc8983d88c8cd2b8d8b3bb1
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-05-23 23:12:28 +0530

    Create CameraFollow.cs script

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+    private CinemachineCamera followCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        followCamera = GetComponent<CinemachineCamera>();
+        followCamera.Follow = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
