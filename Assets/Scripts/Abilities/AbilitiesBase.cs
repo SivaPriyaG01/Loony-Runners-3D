c5176a5f64735ee90eeb019fe7b262ae4d5cdcec
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-05-30 22:31:39 +0530

    Add PowerUp folder and PowerUpBase.cs script

--- Assets/Scripts/Abilities/AbilitiesBase.cs
+++ Assets/Scripts/Abilities/AbilitiesBase.cs
@@ -4,4 +4,9 @@ public class AbilitiesBase : MonoBehaviour
 {
     private string abilityName;
     private int applyForce;
+
+    //attach the script to a gameobject and on collision make player skid and fall
+    //if banana peel and oil bomb: skid and fall
+    //if shock wave fly away and fall
+    //ghost mode: collisions turn off for few seconds
 }
