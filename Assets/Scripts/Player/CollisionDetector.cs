97728ec7a7a0e680dad31ae036b00edefacd01ea
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-02 23:01:09 +0530

    Update CollisionDetector

--- Assets/Scripts/Player/CollisionDetector.cs
+++ Assets/Scripts/Player/CollisionDetector.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 
 public class CollisionDetector : MonoBehaviour
 {
-    public event Action<int> SpeedPowerUp;
-    public event Action<int> PowerJump;
+    public event Action<int,int> SpeedPowerUp;
+    public event Action<int,int> PowerJump;
     public event Action Obstacle;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("SpeedPowerUp"))
         {
-            int speedMultiplier = collision.gameObject.GetComponent<SpeedPowerUp>().SpeedMultiplier;
-            SpeedPowerUp?.Invoke(speedMultiplier);
+            var speedComponent = collision.gameObject.GetComponent<SpeedPowerUp>();
+            int speedMultiplier = speedComponent.SpeedMultiplier;
+            int speedTime = speedComponent.SpeedPowerUpTime;
+            SpeedPowerUp?.Invoke(speedMultiplier,speedTime);
         }
         else if (collision.gameObject.CompareTag("JumpPowerUp"))
         {
-            int jumpMultiplier = collision.gameObject.GetComponent<JumpPowerUp>().JumpMultiplier;
-            PowerJump?.Invoke(jumpMultiplier);
+            var jumpComponent = collision.gameObject.GetComponent<JumpPowerUp>();
+            int jumpMultiplier = jumpComponent.JumpMultiplier;
+            int jumpTime = jumpComponent.JumpPowerUpTime;
+            PowerJump?.Invoke(jumpMultiplier,jumpTime);
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
