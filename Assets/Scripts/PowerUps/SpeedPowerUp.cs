22524ae7a6029bc5005dcb07c9d94918b5002845
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-02 23:01:41 +0530

    Update SpeedPowerUp

--- Assets/Scripts/PowerUps/SpeedPowerUp.cs
+++ Assets/Scripts/PowerUps/SpeedPowerUp.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class SpeedPowerUp : PowerUpBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private int speedMultiplier = 2;
-    public int SpeedMultiplier
-    {
-        get
-        { return speedMultiplier; }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  private int speedMultiplier = 2;
+  public int SpeedMultiplier
+  {
+    get
+    { return speedMultiplier; }
 
-        set
-        {
-          speedMultiplier = value;
-        }
+    set
+    {
+      speedMultiplier = value;
     }
+  }
+
+  private int speedPowerUpTime = 5;
+
+  public int SpeedPowerUpTime
+  {
+    get { return speedPowerUpTime; }
+
+    set
+    {
+      speedPowerUpTime = value;
+    }
+  }
 }
