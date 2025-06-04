92f4aabc7dbd860b2bc8d1231c572bb571ea881e
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-04 22:22:28 +0530

    Update SpeedPowerUp

--- Assets/Scripts/PowerUps/SpeedPowerUp.cs
+++ Assets/Scripts/PowerUps/SpeedPowerUp.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpeedPowerUp : PowerUpBase
 {
   // Start is called once before the first execution of Update after the MonoBehaviour is created
-  public event Action<float,int> SpeedPowerUpEvent;
+  public static event Action<float,int> SpeedPowerUpEvent;
   private float newSpeed;
   public float NewSpeed
   {
