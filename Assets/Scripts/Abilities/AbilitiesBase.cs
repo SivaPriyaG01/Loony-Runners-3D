4d866c8cfe985c5c10ba856f7a5ad77ca6d09538
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-05 22:49:48 +0530

    Update AbilitiesBase.cs

--- Assets/Scripts/Abilities/AbilitiesBase.cs
+++ Assets/Scripts/Abilities/AbilitiesBase.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
 public class AbilitiesBase : MonoBehaviour
 {
-    private string abilityName;
-    private int applyForce;
-
     //attach the script to a gameobject and on collision make player skid and fall
     //if banana peel and oil bomb: skid and fall
     //if shock wave fly away and fall
