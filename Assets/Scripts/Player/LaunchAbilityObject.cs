fbafa3deeb2c1424bf925cd638bce2afa7b1f134
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-06 22:48:35 +0530

    Update LaunchAbilityObject.cs

--- Assets/Scripts/Player/LaunchAbilityObject.cs
+++ Assets/Scripts/Player/LaunchAbilityObject.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class LaunchAbilityObject : MonoBehaviour
 {
-    public static event Action<GameObject> AssignPowerUp;
+    public static event Action<GameObject> AssignAbility;
     public static event Action StartRayCast;
     public static event Action ThrowAbilityObject;
 
@@ -14,18 +14,39 @@ public class LaunchAbilityObject : MonoBehaviour
     private float throwForce;
     private float throeUpwardForce;
     private bool readyToThrow;
+    private float abilityObjectMass;
     void OnEnable()
     {
+        AssignAbility += AssignAbilityObject;
+        StartRayCast += ProjectRayCast;
+        ThrowAbilityObject += Throw;
 
     }
     void OnDisable()
     {
-
+        AssignAbility -= AssignAbilityObject;
+        StartRayCast -= ProjectRayCast;
+        ThrowAbilityObject -= Throw;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
-    
+
+    void AssignAbilityObject(GameObject gameObject)
+    {
+        abilityObject = gameObject;
+        abilityObjectMass = gameObject.GetComponent<Rigidbody>().mass;
+    }
+
+    void ProjectRayCast()
+    {
+
+    }
+
+    void Throw()
+    {
+
+    }
 }
