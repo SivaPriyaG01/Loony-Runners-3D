b7d70f42ac27b9884ba7792848bc6f174b895194
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-10 22:52:36 +0530

    Update AbilityUITriggerManager.cs

--- Assets/Scripts/Managers/AbilityUITriggerManager.cs
+++ Assets/Scripts/Managers/AbilityUITriggerManager.cs
@@ -7,11 +7,13 @@ using System;
 public class AbilityUITriggerManager : MonoBehaviour
 {
     public event Action<GameObject> AssignAbility;
+    public event Action StartRayCast;
     [SerializeField] GameObject bananaPeel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnBananaPeelClick()
     {
-        AssignAbility.Invoke(bananaPeel);
+        AssignAbility?.Invoke(bananaPeel);
+        StartRayCast?.Invoke();
         Debug.Log("AssignAbility invoked, banana peel added");
     }
 }
