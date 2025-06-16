9e49fd72bfd952916e2d149ca7a3fd88dfa0c218
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-16 23:06:05 +0530

    Add ThrowObject.cs
    Testing launching objects

---
+++ Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,36 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThrowObject : MonoBehaviour
+{
+    [SerializeField] GameObject objectToThrow;
+    PlayerInput playerInput;
+    [SerializeField] float throwForce=10f;
+    [SerializeField] bool fire;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 throwDirection;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+         rb = objectToThrow.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fire = playerInput.actions["Attack"].WasPressedThisFrame();
+        if (fire)
+        {
+            OnFireThrowObject();
+        }
+    }
+
+    void OnFireThrowObject()
+    {
+        Instantiate(objectToThrow, startPosition, objectToThrow.transform.rotation);
+        rb.AddRelativeForce(throwDirection * throwForce, ForceMode.Impulse);
+    }
+}
